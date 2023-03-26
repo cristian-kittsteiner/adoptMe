a7ba1bb5813e8c40d822462550eb3be2499eecde
Kryzztian <cdkittsteiner@yahoo.com>
2023-03-26 19:40:59 -0400

    updated search params to include useState

--- 2.Vanilla React/src/App.jsx
+++ 2.Vanilla React/src/App.jsx
@@ -1,15 +1,15 @@
-import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import {createRoot} from "react-dom/client";
+import SearchParams from "./SearchParams.jsx";
 
 const App = () => {
+  return(
   <div>
     <h1>Adopt Me!</h1>
-    <Pet name = "Luna" animal= "Dog" breed = "Havanese"/>
-    <Pet name = "Tsuki" animal= "Cat" breed = "Tsuki"/>
-    <Pet name = "Pepper" animal= "Bird" breed = "Parakeet"/>
+    <SearchParams />
   </div>
+  )
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(<App />);
