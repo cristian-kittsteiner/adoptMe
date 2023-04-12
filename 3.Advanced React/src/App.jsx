b24ad3bafe2318746e50eb9f0e96c7d6b4327d17
Kryzztian <cdkittsteiner@yahoo.com>
2023-04-12 13:48:15 -0400

    initializing part 3

---
+++ 3.Advanced React/src/App.jsx
@@ -0,0 +1,23 @@
+import {createRoot} from "react-dom/client";
+import {BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import SearchParams from "./SearchParams.jsx";
+import Details from "./Details.jsx";
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
+      <Routes>
+        <Route path = "/details/:id" element = {<Details />} />
+        <Route path = "/" element = {<SearchParams />} />
+      </Routes>
+    </BrowserRouter>
+  )
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
